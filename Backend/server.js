5a31c67db031b915720c130e5cea10548973a9d1
Prabhat SInghal <prabhatsinghal869@gmail.com>
2025-02-17 20:42:42 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const http=require('http');
+
+const app=require('./app'); 
+const port=process.env.PORT || 4000;
+
+
+const server=http.createServer(app);
+
+if (require.main === module) {
+    server.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
